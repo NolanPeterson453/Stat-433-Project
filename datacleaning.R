93ccfe23790a1eec4f0b26cdbdc6635bfcd45393
NolanPeterson453 <79056951+NolanPeterson453@users.noreply.github.com>
2021-12-05 16:27:30 -0600

    more data cleaning

--- datacleaning.R
+++ datacleaning.R
@@ -60,7 +60,7 @@ OES_19_raw <-OES_19_raw%>%
   select(-AREA_TYPE,-NAICS,-NAICS_TITLE,-I_GROUP,-OWN_CODE,-PCT_TOTAL)
 
 
-## Stack all 5 years into one data frame
+## Stack both years into one data frame
 comb_OES <- rbind(OES_16_raw, OES_19_raw)
 
 ## Function to create header
@@ -89,14 +89,67 @@ comb_ACS_OES_KEY <- mean_age_occp %>%
 comb_ACS_OES_KEY <- comb_ACS_OES_KEY %>% 
   select(YEAR, OCCP, mean_age, unemployed_rate,
          OCC_CODE, OCC_TITLE, TOT_EMP, EMP_PRSE, 
-         JOBS_1000, H_MEAN, A_MEAN, A_MEDIAN, MEAN_PRSE)
+         JOBS_1000, H_MEAN, A_MEAN, MEAN_PRSE)
 
 ## Filter out rows where sample size is too small to collect unemployment data
 ## And filter out rows where OES and ACS have no comparison 
 comb_ACS_OES_KEY <- comb_ACS_OES_KEY %>% 
   filter(unemployed_rate != 0,
+         unemployed_rate != 100,
          !is.na(OCC_TITLE))
+
+## Assign NA to missing values in the data
+## Assign data types to numeric variables 
+## Remove NA's from data
+comb_ACS_OES_KEY[comb_ACS_OES_KEY == "*" | comb_ACS_OES_KEY == "**"] <- NA
+comb_ACS_OES_KEY[,7:12] <- as.data.frame(sapply(comb_ACS_OES_KEY[,7:12], as.numeric))
+comb_ACS_OES_KEY <- comb_ACS_OES_KEY %>% 
+  filter(!is.na(OCCP))
+comb_ACS_OES_KEY$YEAR <- as.factor(comb_ACS_OES_KEY$YEAR)
+
+## Reconcile difference in acs coding system oes coding system
+cleaned_data <- comb_ACS_OES_KEY %>% 
+  group_by(YEAR, OCCP) %>% 
+  summarise(mean_age = mean(mean_age, na.rm = TRUE),
+            unemployed_rate = mean(unemployed_rate, na.rm = TRUE),
+            total_employ = sum(TOT_EMP, na.rm = TRUE),
+            employ_prse = mean(EMP_PRSE, na.rm = TRUE),
+            jobs_per_1000 = sum(JOBS_1000, na.rm = TRUE),
+            mean_hourly = mean(H_MEAN, na.rm = TRUE),
+            mean_annual = mean(A_MEAN, na.rm = TRUE),
+            mean_prse = mean(MEAN_PRSE, na.rm = TRUE))
+
+## Create a clean 2016 data
+cleaned_data_2016 <- cleaned_data %>% 
+  filter(YEAR == 2016 )
+
+## Create a clean 2019 data
+cleaned_data_2019 <- cleaned_data %>% 
+  filter(YEAR == 2019 )
+
+## Join these two data set to get Year specific columns (x = 2016, y = 2019)
+## could possibly be done with 'pivot_wider' but I had no luck
+cleaned_data <- inner_join(cleaned_data_2016, cleaned_data_2019, by = "OCCP") 
+
+
+
 
 ################################################################################################
 
+
+# jobs_plot <- ggplot(data = comb_ACS_OES_KEY, aes(x = mean_age, 
+#                                     y = unemployed_rate, 
+#                                     color = A_MEAN,
+#                                     size = JOBS_1000)) +
+#   geom_point() + 
+#   labs(title = "Average Age vs Unemployment Rate by Occupation",
+#        y = "Umeployment Rate",
+#        x = "Mean Age of Workers in Occupation", 
+#        color = "Mean Annual Salary of Occupation",
+#        size = "Number of Total Jobs per Thousand in Occupation") +
+#   scale_color_continuous(breaks=c(0,50000,100000,150000),
+#                          labels=c("$0","$50,000","$100,000", "$150,000")) +
+#   facet_wrap(~YEAR)
+#   
+# ggsave('jobs_plot.png', plot = jobs_plot, units = "in", width = 20, height = 10)
 
